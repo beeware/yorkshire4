0d92369e4a2f72321a1b0679c34bbd01bb8c61f4
Rosalind Fulton <rosalind.fulton@acmi.net.au>
2017-12-30 21:37:37 +1100

    thrown together version of the first program in computer battlegames

--- docs/books/computer-games/computer-battle-games/robot_missile/robot_missile.py
+++ docs/books/computer-games/computer-battle-games/robot_missile/robot_missile.py
@@ -1,1 +1,31 @@
+import random
+import string
 
+print('ROBOT MISSLE' \
+      '\n'
+      '\nType the correct code letter (A-Z) to defuse the missle.' \
+      '\nYou have 4 chances.')
+
+code = random.choice(string.ascii_lowercase)
+
+guesses = 0
+success = False
+
+while not success and guesses < 4:
+    guess = input('What is your guess? ')
+    guesses += 1
+
+    if guess.lower() == code:
+        success = True
+    if guess < code:
+        print('The code is later than %s in the alphabet' % guess)
+    if guess > code:
+        print('The code is earlier than %s in the alphabet' % guess)
+
+if success:
+    print('TICK... FIZZ... CLICK...'
+          '\nYou did it!')
+else:
+    print('BOOOOOOOOMMM...' \
+          '\nYou blew it!' \
+          '\nThe correct code was \'%s\'.' % code)
