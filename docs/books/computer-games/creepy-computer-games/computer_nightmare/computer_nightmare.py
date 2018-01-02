beb5275e4e603ae6fe3df9fa46f67ce83d9c4a6e
Russell Keith-Magee <russell@keith-magee.com>
2018-01-03 07:58:51 +0800

    Clean up some recent examples.

--- docs/books/computer-games/creepy-computer-games/computer_nightmare/computer_nightmare.py
+++ docs/books/computer-games/creepy-computer-games/computer_nightmare/computer_nightmare.py
@@ -1,4 +1,3 @@
-
 # Import 'random' so we can generate random numbers.
 import random
 
@@ -7,40 +6,40 @@ score = 300
 
 # List of comments for making the game a more fun!
 comments = [
-        "** MICROS RULE! **", "*PEOPLE ARE SUPID*",
-        "+A ROBOT FOR PRESIDENT!", "!COMPUTERS ARE GREAT!",
-        "*I'M BETTER THAN YOU!*"
-        ]
+    '** MICROS RULE! **',
+    '*PEOPLE ARE SUPID*',
+    '+A ROBOT FOR PRESIDENT!',
+    '!COMPUTERS ARE GREAT!',
+    'I AM BETTER THAN YOU!',
+]
+
+# Print a heading
+print('  Number   Score')
+print('~~~~~~~~~~~~~~~~')
 
 # Loop while the score is not above 500 i.e you have won,
 # or less than 0 means you have lost the game
 while score > 0 and score < 500:
-    # Number that will be appeared on screen
+    # Number that will appear on screen
     number = random.randint(1, 9)
-    print("   %d" % (number))
-    print("                     %d" % (score))
+    print('    %4d    %4d' % (number, score))
 
-    if random.randint(1, 9) > 5:
+    if random.random() > 0.5:
         print(comments[int(score/100)])
-
     else:
         if score < 60:
-            print("THERE IS NO HOPE")
+            print('THERE IS NO HOPE')
         elif score > 440:
-            print("URK! HELP!!")
+            print('URK! HELP!!')
 
-    print("ENTER THE ANSWER")
-    answer = int(input())
+    answer = int(input('Enter your guess:'))
     # If you have guessed right increase the score
     if answer == number:
         score = score + number * 2
-
     else:
         score = score - 10
 
-
 if score < 0:
-    print("YOU'RE NOW MY SLAVE")
-
+    print('YOU ARE NOW MY SLAVE')
 else:
-    print("OKAY! YOU WIN (THIS TIME)")
+    print('OK... you win... (this time)')
