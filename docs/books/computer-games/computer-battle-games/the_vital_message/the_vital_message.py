2a443a8c0f24ea5b3b9b0bf60899710df9e59825
ashwani010 <31697634+ashwani010@users.noreply.github.com>
2018-01-02 15:38:14 +0530

    corrected the identations

--- docs/books/computer-games/computer-battle-games/the_vital_message/the_vital_message.py
+++ docs/books/computer-games/computer-battle-games/the_vital_message/the_vital_message.py
@@ -1,6 +1,6 @@
 # Import 'random' so we can generate random numbers.
 # The 'string' module gives us access to string-related utilities
-#The 'time' module helps us to wait for some time
+# The 'time' module helps us to wait for some time
 
 
 import random
@@ -8,36 +8,40 @@ import string
 import time
 print("VITAL MESSAGE")
 
-#Variable to store the difficulty i.e length of string
-D=0
+# Variable to store the difficulty i.e length of string
+level_difficulty = 0
 
-#Loop until the right difficulty level is enterred
-while D<4 or D>10:
+# Loop until the right difficulty level is enterred
+while level_difficulty < 4 or level_difficulty > 10:
     print("HOW DIFFICULT? (4-10)")
-    x=int(input())
-    D=x
+    x = int(input())
+    level_difficulty = x
 
-#Function to clear the screen
+# Function to clear the screen
+# This simply clears the screen by printing 64 blank lines
+
+
 def clrsc():
-    print('\n'*64)
+    print('\n' * 64)
 
-#Variable to store the secret message
-message=""
 
-for i in range(0,D):
-    message+=(random.choice(string.ascii_letters))
+# Variable to store the secret message
+message = ""
+
+for i in range(0, level_difficulty):
+    message += (random.choice(string.ascii_letters))
 
 print("SEND THIS MESSAGE:")
-print (message)
+print(message)
 
-#Displaying the secret message in accordance of the difficulty level
-time.sleep(0.5*D)
+# Displaying the secret message in accordance of the difficulty level
+time.sleep(0.5 * level_difficulty)
 clrsc()
 
 print("Enter the secret code that need to be transferred")
-code=input()
+code = input()
 
-if(code==message):
+if(code == message):
     print("MESSAGE CORRECT")
     print("THE WAR IS OVER")
 else:
